c8ff0ac2ab566f37cd055178c8efe230cb4ae53e
hossein <hkhani1384@gmail.com>
2026-07-07 01:38:59 +0330

    Add fix

--- middleware/rateLimit.js
+++ middleware/rateLimit.js
@@ -3,6 +3,7 @@ const { rateLimit } = require("express-rate-limit");
 const authLimiter = rateLimit({
   windowMs: 60 * 60 * 1000,
   limit: 10,
+  trustProxy: true,
   handler: function (req, res) {
     res.status(429).json({
       error: "تعداد تلاش‌های ناموفق زیاد است، لطفاً بعداً تلاش کنید",
@@ -12,6 +13,7 @@ const authLimiter = rateLimit({
 const changePasswordLimiter = rateLimit({
   windowMs: 60 * 60 * 1000,
   limit: 5,
+  trustProxy: true,
   handler: function (req, res) {
     res.status(429).json({
       error: "تعداد تلاش‌های ناموفق زیاد است، لطفاً بعداً تلاش کنید",
