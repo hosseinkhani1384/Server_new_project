6a5781e1005293a4b045a10cb090cf9a80cdde57
hossein <hkhani1384@gmail.com>
2026-07-07 03:37:43 +0330

    delete cors

--- server.js
+++ server.js
@@ -13,26 +13,13 @@ const { authorizationUser } = require("./middleware/user.middleware");
 const app = express();
 const port = process.env.PORT || 2000;
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://www.hosseinkhani20.ir",
-      "https://hosseinkhani20.ir",
-      "https://new-project-mu-ten.vercel.app",
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
-app.set('trust proxy', 1); 
+app.set("trust proxy", 1);
 app.use("/api/auth", apiRouter);
-app.use("/api/user", authorizationUser ,userRouter);
+app.use("/api/user", authorizationUser, userRouter);
 
 app.get("/", (req, res) => {
   res.status(200).json({ ok: true });
